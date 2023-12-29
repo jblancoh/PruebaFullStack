4e3fd063daed2fddbdc2cefe148090451e286c25
jonathan <zlatanjon@hotmail.com>
2023-12-29 09:54:55 -0600

    Feat(Config): Se agregan variables de entorno

--- api-fullstack/src/app.module.ts
+++ api-fullstack/src/app.module.ts
@@ -4,18 +4,31 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { Product } from './products/entities/product.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
+      }),
       TypeOrmModule.forRoot({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'db_crud',
+        host: process.env.POSTGRES_HOST,
+        port: parseInt(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
         entities: [Product],
         synchronize: true,
+        ssl: process.env.POSTGRES_SSL === 'true',
+        extra: {
+          ssl: 
+            process.env.POSTGRES_SSL === 'true'
+              ? {
+                  rejectUnauthorized: false,
+                }
+              : null,
+        },
       }),
       ProductsModule,
     ],
